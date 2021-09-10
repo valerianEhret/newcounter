45a9e1342a15510532e98c140b148c834c972367
valerianEhret <vkadirovv@gmail.com>
2021-09-10 21:37:57 +0200

    Updates

---
+++ precache-manifest.aae8df9cb5cc154ab4c7db84c3eb9b8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236f5ce7eaecd30116b8a44951397b98",
+    "url": "/newcounter/index.html"
+  },
+  {
+    "revision": "d60603d6a97051dff935",
+    "url": "/newcounter/static/css/main.261abf2c.chunk.css"
+  },
+  {
+    "revision": "5eb3cbd8e19110beafa4",
+    "url": "/newcounter/static/js/2.e114cbdc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/newcounter/static/js/2.e114cbdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60603d6a97051dff935",
+    "url": "/newcounter/static/js/main.247cd126.chunk.js"
+  },
+  {
+    "revision": "b7df5f7daf25adc9c2c0",
+    "url": "/newcounter/static/js/runtime-main.90596302.js"
+  }
+]);
