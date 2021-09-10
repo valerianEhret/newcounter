d14ee35f1a24208d7a268911f914bc178da76766
valerianEhret <vkadirovv@gmail.com>
2021-09-10 22:47:20 +0200

    refactroing

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,6 @@ import {SettingsDisplay} from "./common/SettingsDisplay/SettingsDisplay";
 import {Button} from "./common/Button/Button";
 
 function App() {
-    //тип экспортируется из StoreRedux, а второй тип это тип инициализационного стейта, параметры callback state => state.counter
-    // типизация useSelectora 1 параметр это тип Стора полыйчный из редьюсера, вторым параметром, то что он возвращает, типизация иницилазционного стейта.
     const counter = useSelector<AppRootStateType, StateType>( state=>state.counter)
 
     const disableSetButton = counter.counterState.startValue < 0 || counter.counterState.maxValue < 0 || counter.counterState.startValue >= counter.counterState.maxValue
@@ -26,20 +24,23 @@ function App() {
 
     return (
         <div className="container">
-                <div className="counter">
-                    {/*<div className="display">*/}
+                <div className="counterBlock">
                         <Display state={counter}/>
-                    {/*</div>*/}
-                    <div className="display-buttons">
+                    <div className="buttons">
                         <Button buttonTitle={'INC'} buttonDisabled={disableIncButton} actionCreator={incrementValueAC}/>
                         <Button buttonTitle={'RESET'} buttonDisabled={disableResetButton} actionCreator={resetSetValueAC}/>
                     </div>
 
                 </div>
-                <div className="settings">
-                    <SettingsDisplay title={'START VALUE'} state={counter} actionCreator={changeStartValueAC} value={counter.counterState.startValue} />
-                    <SettingsDisplay title={'MAX VALUE'}  state={counter} actionCreator={changeMaxValueAC} value={counter.counterState.maxValue}/>
-                    <Button buttonTitle={'SET'} buttonDisabled={disableSetButton} actionCreator={resetSetValueAC}/>
+                <div className="settingsBlock">
+                    <div className="settings">
+                        <SettingsDisplay title={'START VALUE'} state={counter} actionCreator={changeStartValueAC} value={counter.counterState.startValue} />
+                        <SettingsDisplay title={'MAX VALUE'}  state={counter} actionCreator={changeMaxValueAC} value={counter.counterState.maxValue}/>
+                    </div>
+                    <div className="buttons">
+                        <Button buttonTitle={'SET'} buttonDisabled={disableSetButton} actionCreator={resetSetValueAC}/>
+                    </div>
+
                 </div>
         </div>
     );
